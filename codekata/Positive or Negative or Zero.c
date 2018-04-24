97ad4ea4ef36b64105a0afc17b499c8af601c923
AishSaag <38650933+AishSaag@users.noreply.github.com>
2018-04-24 12:27:05 +0530

    Create Positive or Negative or Zero.c

---
+++ codekata/Positive or Negative or Zero.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    int num;
+    printf("enter the number:");
+    scanf("%d",&num);
+    if (num>0)
+        printf("The number is positive");
+    else if (num<0)
+        printf("The number is negative");
+    else 
+     printf("The number is zero");
+    return 0;
+    
+}
