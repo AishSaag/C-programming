54cfa71bd11338fa1b2ec2960898e4cc45d667e0
AishSaag <38650933+AishSaag@users.noreply.github.com>
2018-04-24 14:52:01 +0530

    Update largest.c

--- codekata/largest.c
+++ codekata/largest.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+int main ()
+{
+    double a,b,c;
+    scanf("%lf %lf %lf",&a,&b,&c);
+    if (a>b && a>c)
+        printf("%2f is the largest number",a);
+    if (b>a && b>c)
+        printf("%2f is the largest number",b);
+    if (c>a && c>b)
+        printf("%2f is the largest number",c);
+     
+}
